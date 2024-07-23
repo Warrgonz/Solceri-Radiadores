244f643f870f225231819a61ee94201706042545
acastrog38 <acastrog38@gmail.com>
2024-07-23 00:17:16 -0600

    Correcciones en login. Se agregan visualizaciones de alerta correctas con jsonify y ajax. Se solucionan bugs de password reset. ahora funciona correctamente. Metodos de recovery agregados pero falta arreglar rutas correctas.

--- utils/db.py
+++ utils/db.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def init_db(app: Flask):
     try: # Cambiar contraseña de la db
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345@localhost/solceri'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/solceri'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         
         db.init_app(app)
@@ -16,3 +16,4 @@ def init_db(app: Flask):
     
     except Exception as e:
         print(f"Error connecting to the database: {str(e)}")
+
