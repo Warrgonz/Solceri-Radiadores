547b540777a72b0352afb2c8eeba1c55da524f63
acastrog38 <acastrog38@gmail.com>
2024-07-23 22:26:59 -0600

    Crud grupos, fixed, multiples usuarios se pueden asignar a grupos

--- models/grupos.py
+++ models/grupos.py
@@ -1,11 +1,20 @@
 # models/grupos.py
 from utils.db import db
 
+# Tabla miselanea
+grupo_usuario = db.Table('grupo_usuario',
+    db.Column('grupo_id', db.Integer, db.ForeignKey('grupos.id_grupo'), primary_key=True),
+    db.Column('usuario_id', db.Integer, db.ForeignKey('usuarios.id_usuario'), primary_key=True)
+)
+
 class Grupos(db.Model):
     __tablename__ = 'grupos'
     id_grupo = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(255))
     descripcion = db.Column(db.String(255))
+    
+    # relacion con usuarios
+    usuarios = db.relationship('Usuarios', secondary=grupo_usuario, backref=db.backref('grupos', lazy='dynamic'))
 
     def __init__(self, nombre, descripcion):
         self.nombre = nombre
--- routes/grupos.py
+++ routes/grupos.py
@@ -4,6 +4,8 @@ from flask import Blueprint, render_template, request, redirect, url_for, jsonif
 from models.grupos import Grupos
 from utils.db import db
 from utils.auth import login_required, role_required
+from models.usuarios import Usuarios
+
 
 grupos_bp = Blueprint('grupos', __name__)
 
@@ -15,22 +17,27 @@ def grupos():
     grupos = Grupos.query.all()
     return render_template('grupos.html', grupos=grupos)
 
-# Ruta para crear un nuevo equipo
 @grupos_bp.route('/grupos/crear', methods=['GET', 'POST'])
 def grupos_crear():
     if request.method == 'POST':
         nombre = request.form['nombre']
         descripcion = request.form['descripcion']
+        usuarios_ids = request.form.getlist('usuarios')  # Obtén los IDs de los usuarios seleccionados
 
         nuevo_grupo = Grupos(nombre=nombre, descripcion=descripcion)
+        for usuario_id in usuarios_ids:
+            usuario = Usuarios.query.get(int(usuario_id))
+            if usuario:
+                nuevo_grupo.usuarios.append(usuario)
+
         db.session.add(nuevo_grupo)
         db.session.commit()
 
-        return redirect(url_for('grupos.grupos'))  # Redirige a la lista de equipos
+        return redirect(url_for('grupos.grupos'))  # Redirige a la lista de grupos
 
-    return render_template('grupos_crear.html')
+    usuarios = Usuarios.query.filter(Usuarios.id_rol.in_([1, 2])).all()
+    return render_template('grupos_crear.html', usuarios=usuarios)
 
-# Ruta para editar un equipo específico
 @grupos_bp.route('/grupos/editar/<int:id_grupo>', methods=['GET', 'POST'])
 def grupos_editar(id_grupo):
     grupo = Grupos.query.get(id_grupo)
@@ -38,20 +45,35 @@ def grupos_editar(id_grupo):
         if grupo:
             grupo.nombre = request.form['nombre']
             grupo.descripcion = request.form['descripcion']
+            grupo.usuarios.clear()  # Limpia usuarios actuales
+            usuarios_ids = request.form.getlist('usuarios')  # Obtén los IDs de los usuarios seleccionados
+            for usuario_id in usuarios_ids:
+                usuario = Usuarios.query.get(int(usuario_id))
+                if usuario:
+                    grupo.usuarios.append(usuario)
             db.session.commit()
             return jsonify({'message': 'El grupo ha sido actualizado exitosamente'})
         else:
             return jsonify({'error': 'El grupo no existe'}), 404
 
-    return render_template('grupos_editar.html', grupo=grupo)
+    usuarios = Usuarios.query.filter(Usuarios.id_rol.in_([1, 2])).all()
+    return render_template('grupos_editar.html', grupo=grupo, usuarios=usuarios)
 
-# Ruta para eliminar un equipo específico
 @grupos_bp.route('/grupos/eliminar/<int:id_grupo>', methods=['POST'])
 def grupos_eliminar(id_grupo):
     grupo = Grupos.query.get(id_grupo)
     if grupo:
         db.session.delete(grupo)
         db.session.commit()
-        return jsonify({'message': 'El grupo ha sido eliminado exitosamente'})
+        return redirect(url_for('grupos.grupos'))
     else:
         return jsonify({'error': 'El grupo no existe'}), 404
+    
+@grupos_bp.route('/grupos/detalles/<int:id_grupo>')
+@login_required
+@role_required([1])
+def grupos_detalles(id_grupo):
+    grupo = Grupos.query.get(id_grupo)
+    if grupo:
+        return render_template('grupos_detalles.html', grupo=grupo)
+    return jsonify({'error': 'El grupo no existe'}), 404   
